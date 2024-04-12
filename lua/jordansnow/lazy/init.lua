9b8242e290a5eb3cbf25d140839bf559428206ec
Jordan Snow <jordan@snowdev.co>
2024-04-11 21:15:58 -0600

    feat: added diffview for git diff handling

--- lua/jordansnow/lazy/init.lua
+++ lua/jordansnow/lazy/init.lua
@@ -19,4 +19,24 @@ return {
       require("Comment").setup()
     end
   },
+
+  -- enabling devicons
+  {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>dv", function()
+        vim.cmd.DiffviewOpen()
+      end)
+      vim.keymap.set("n", "<leader>dc", function()
+        vim.cmd.DiffviewClose()
+      end)
+      vim.keymap.set("n", "<leader>df", function()
+        vim.cmd.DiffviewToggleFiles()
+      end)
+    end
+  },
 }
