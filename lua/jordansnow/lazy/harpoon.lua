a21004bde552af35c42c0ee1403a3cae80d396b9
Jordan Snow <jordan@snowdev.co>
2024-04-30 19:44:58 -0600

    feat: change harpoon window to telescope
    Added config to use telescope as the harpoon previewer and picker - plan
    to use it for a little while and see if it's something I like.  If not,
    return back to the previous commit.

--- lua/jordansnow/lazy/harpoon.lua
+++ lua/jordansnow/lazy/harpoon.lua
@@ -1,8 +1,25 @@
 return {
   "theprimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = { "plenary" },
+  dependencies = { "plenary", "telescope" },
   config = function()
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
+    end
     local harpoon = require("harpoon")
     harpoon:setup({
       settings = {
@@ -10,10 +27,9 @@ return {
         -- sync_on_ui_close = true,
       }
     })
+
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<C-e>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end)
 
     vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<leader>k", function() harpoon:list():select(2) end)
--- lua/jordansnow/lazy/telescope.lua
+++ lua/jordansnow/lazy/telescope.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
+  name = "telescope",
   dependencies = {
     "plenary"
   },
